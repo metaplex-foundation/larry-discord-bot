99cc769aa5e427b6f6763631115f18bcb24a1c1e
stegaBOB <sammyisaacharris@gmail.com>
2021-12-04 17:18:37 -0500

    fixed untracked files

---
+++ index.ts
@@ -0,0 +1,13 @@
+// Require neccesary discord.js classes and dotenv
+const dotenv = require('dotenv').config();
+const { Client, Intents } = require('discord.js');
+
+// Create a new discord client
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(process.env.DISCORD_TOKEN);
